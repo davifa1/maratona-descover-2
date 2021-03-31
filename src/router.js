6b8ae15d014ae87c3366a5380e27da2a7dbe22fa
davifa1 <davihenriquefavaro@gmail.com>
2021-03-31 18:16:18 -0300

    aula 02 parte 1

--- src/router.js
+++ src/router.js
@@ -5,19 +5,34 @@ const view = __dirname + "/views/"
 
 const profile = {
     name: "Davi Henrique",
-    avatar: "https://avatars.githubusercontent.com/u/63513686?v=4",
+    avatar: "https://github.com/davifa1.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 4,
     "vacation-per-year": 4,
 }
 
+// ESTRUTURA DE DADOS:
+//array
+//objetp
+
+const jobs = []
+
+//criar as rotas
+
+//request / response
 routes.get('/', (req, res) => {
     return res.render(view + 'index')
 })
 
 routes.get('/job', (req, res) => {
     return res.render(view + 'job')
+})
+routes.post('/job', (req, res) => {
+    //PUSH(colocar/enviar/enpurar ):
+    //colocar coisas dentro do array
+    jobs.push(req.body)
+    return res.redirect('/')
 })
 routes.get('/job/edit', (req, res) => {
     return res.render(view + 'job-edit')
@@ -26,4 +41,5 @@ routes.get('/profile', (req, res) => {
     return res.render(view + 'profile', { profile })
 })
 
+// exportar para as rotas para para todo o projeto
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const server = express();
 const routes = require('./router');
 
+// USE: colocar/abilitar configuraçao no server
+
+// usar o motor de view o ejs
 server.set('view engine', 'ejs')
-
+// habilitar arquivos static
 server.use(express.static('public'))
-
+//usar o req.body
+server.use(express.urlencoded({ extended: true }))
+// chamar as rotas
 server.use(routes)
-
+// ligar o servidor
 server.listen(3000)
